bd60b6d9401027278fcc963709abf78fd4dfb915
ICantCode3 <76601196+ICantCode3@users.noreply.github.com>
2023-06-14 21:04:38 -0400

    Add files via upload

--- TwoSum.py
+++ TwoSum.py
@@ -1,12 +1,12 @@
-nums = [-1, -2, -3, -4, -5]
-target = -8
 class Solution(object):
-    def twoSum(nums, target):
-        seen = {}
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
         for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in seen:
-                return [seen[diff], i]
-            else:
-                seen[nums[i]] = i
-    print(twoSum(nums, target))
+            for j in range(len(nums)):
+                if i != j: 
+                    if nums[i] + nums[j] == target:
+                        return [i, j]
